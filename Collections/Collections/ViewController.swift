49a1cfb8d918f5442e336b6536cb56d48ccd6290
Oleh <lyuclyan.oleg14@gmail.com>
2024-09-30 16:35:57 +0300

    Created Project.

---
+++ Collections/Collections/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Collections
+//
+//  Created by Mac on 30.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
