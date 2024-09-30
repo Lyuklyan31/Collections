0ca8d343555c74d334e36bbc2bffca1379e3d9f8
Oleh <lyuclyan.oleg14@gmail.com>
2024-09-30 17:41:46 +0300

    Created tableView.

--- Collections/Collections/MainViewController.swift
+++ Collections/Collections/MainViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    private let tableView = UITableView()
+    private let collections = ["Array", "Set", "Dictionary"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupTableView()
     }
+    func setupTableView() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        tableView.dataSource = self
+    }
+    
+}
 
+extension MainViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return collections.count
+  }
 
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    cell.textLabel?.text = collections[indexPath.row]
+    return cell
+  }
 }
 
