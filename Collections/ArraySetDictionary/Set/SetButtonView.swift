2d214cf7c9f06cb7a06e82d369da3852d9351f7e
Oleh <lyuclyan.oleg14@gmail.com>
2024-10-08 00:00:02 +0300

    Сreated a universal button.

---
+++ Collections/ArraySetDictionary/Set/SetButtonView.swift
@@ -0,0 +1,56 @@
+import UIKit
+
+class SetButton: UIView {
+
+    let button = UIButton()
+    let resultLabel = UILabel()
+    private let title: String
+    
+    var buttonAction: (() -> Void)?
+    
+    init(_ title: String) {
+        self.title = title
+        super.init(frame: .zero)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupUI() {
+        setupButton()
+        setupResultLabel()
+    }
+    
+    func setupButton() {
+        button.setTitle(title, for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 15)
+        
+        addSubview(button)
+        button.snp.makeConstraints {
+            $0.verticalEdges.equalToSuperview()
+            $0.horizontalEdges.equalToSuperview()
+        }
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+    }
+        
+    func setupResultLabel() {
+        
+        resultLabel.text = ""
+        resultLabel.textAlignment = .center
+        resultLabel.isHidden = true
+        
+        addSubview(resultLabel)
+        resultLabel.snp.makeConstraints {
+            $0.top.equalTo(button.snp.bottom).offset(16)
+            $0.horizontalEdges.equalToSuperview()
+            $0.bottom.greaterThanOrEqualToSuperview()
+        }
+    }
+    
+    @objc private func buttonTapped() {
+        buttonAction?()
+    }
+}
