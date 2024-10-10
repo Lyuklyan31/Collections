006dfb1be82f252ccbd8d177e88ea54dda510485
Oleh <lyuclyan.oleg14@gmail.com>
2024-10-10 14:50:15 +0300

    Updated methods ArrayService.

--- Collections/ArraySetDictionary/Array/ArrayService.swift
+++ Collections/ArraySetDictionary/Array/ArrayService.swift
@@ -1,242 +1,67 @@
 import Foundation
 
-class ArrayService {
-    // MARK: - Properties
-    var myArray = [Int]()
-    //Arrays for UnitTests
-    var createdArrayResults = [Int]()
-    var insertedAtBeginningOneByOneResults = [Int]()
-    var insertedAtBeginningAllAtOnceResults = [Int]()
-    var insertedInMiddleOneByOneResults = [Int]()
-    var insertedInMiddleAllAtOnceResults = [Int]()
-    var insertedAtEndOneByOneResults = [Int]()
-    var insertedAtEndAllAtOnceResults = [Int]()
-    var removedAtBeginningOneByOneResults = [Int]()
-    var removedAtBeginningAllAtOnceResults = [Int]()
-    var removedInMiddleOneByOneResults = [Int]()
-    var removedInMiddleAllAtOnceResults = [Int]()
-    var removedAtEndOneByOneResults = [Int]()
-    var removedAtEndAllAtOnceResults = [Int]()
+struct ArrayOperationResult {
+    let time: UInt64
+    let arraySnapshot: [Int]
+}
 
+class ArrayService {
+    
     // MARK: - Array Creation
+    var myArray = [Int]()
+    
     @discardableResult
-    func createArray() async -> String {
+    func createArray() -> ArrayOperationResult {
         let startTime = DispatchTime.now()
-        
         for number in 0...9_999_999 {
             myArray.append(number)
         }
-        
         let endTime = DispatchTime.now()
-        let nanoseconds = endTime.uptimeNanoseconds - startTime.uptimeNanoseconds
-        let seconds = Double(nanoseconds) / 1_000_000_000
-        
-        let formattedTime = String(format: "%.3f", seconds)
-        createdArrayResults = myArray
-        
-        return "Array creation time: \(formattedTime) ms."
+        let time = endTime.uptimeNanoseconds - startTime.uptimeNanoseconds
+        return ArrayOperationResult(time: time, arraySnapshot: myArray)
     }
-
-    // MARK: - Insert at Beginning
-    func insertAtBeginningArrayOneByOne() async -> String {
+    
+    // MARK: - Insert OneByOne
+    func insertOneByOne(at index: Int) -> ArrayOperationResult {
         var array = myArray
         let startTime = DispatchTime.now()
-        
         for number in 1...1000 {
-            array.insert(number, at: 0)
+            array.insert(number, at: index)
         }
         let endTime = DispatchTime.now()
-        let nanoseconds = endTime.uptimeNanoseconds - startTime.uptimeNanoseconds
-        let seconds = Double(nanoseconds) / 1_000_000_000
-        
-        insertedAtBeginningOneByOneResults = array
-        
-        return "Insertion time: \(seconds) ms."
+        let time = endTime.uptimeNanoseconds - startTime.uptimeNanoseconds
+        return ArrayOperationResult(time: time, arraySnapshot: array)
     }
-
-    func insertAtBeginningArray() async -> String {
+    
+    // MARK: - Insert Once
+    func insertOnce(at index: Int) -> ArrayOperationResult {
         var array = myArray
         let startTime = DispatchTime.now()
-        
-        array.insert(contentsOf: 1...1000, at: 0)
-        
+        array.insert(contentsOf: 1...1000, at: index)
         let endTime = DispatchTime.now()
-        let nanoseconds = endTime.uptimeNanoseconds - startTime.uptimeNanoseconds
-        let seconds = Double(nanoseconds) / 1_000_000_000
-        
-        insertedAtBeginningAllAtOnceResults = array
-        
-        return "Insertion time: \(seconds) ms."
+        let time = endTime.uptimeNanoseconds - startTime.uptimeNanoseconds
+        return ArrayOperationResult(time: time, arraySnapshot: array)
     }
-
-    // MARK: - Insert in Middle
-    func insertInMiddleArrayOneByOne() async -> String {
+    
+    // MARK: - Remove OneByOne
+    func removeOneByOne(at index: Int, count: Int) -> ArrayOperationResult {
         var array = myArray
         let startTime = DispatchTime.now()
-        
-        for number in 1...1000 {
-            array.insert(number, at: 4_999_999)
+        for _ in 0..<count {
+            array.remove(at: index)
         }
         let endTime = DispatchTime.now()
-        
-        insertedInMiddleOneByOneResults = array
-        
-        let nanoseconds = endTime.uptimeNanoseconds - startTime.uptimeNanoseconds
-        let seconds = Double(nanoseconds) / 1_000_000_000
-        
-        return "Insertion time: \(seconds) ms."
+        let time = endTime.uptimeNanoseconds - startTime.uptimeNanoseconds
+        return ArrayOperationResult(time: time, arraySnapshot: array)
     }
 
-    func insertInMiddleArray() async -> String {
+    // MARK: - Remove Once
+    func removeOnce(at index: Int, count: Int) -> ArrayOperationResult {
         var array = myArray
         let startTime = DispatchTime.now()
-        
-        array.insert(contentsOf: 1...1000, at: 4_999_999)
-        
+        array.removeSubrange(index..<index + count)
         let endTime = DispatchTime.now()
-        let nanoseconds = endTime.uptimeNanoseconds - startTime.uptimeNanoseconds
-        let seconds = Double(nanoseconds) / 1_000_000_000
-        
-        insertedInMiddleAllAtOnceResults = array
-        
-        return "Insertion time: \(seconds) ms."
-    }
-
-    // MARK: - Insert at End
-    func insertAtEndArrayOneByOne() async -> String {
-        var array = myArray
-        let startTime = DispatchTime.now()
-        
-        for number in 1...1000 {
-            array.insert(number, at: 9_999_999)
-        }
-        let endTime = DispatchTime.now()
-        
-        insertedAtEndOneByOneResults = array
-        
-        let nanoseconds = endTime.uptimeNanoseconds - startTime.uptimeNanoseconds
-        let seconds = Double(nanoseconds) / 1_000_000_000
-        
-        return "Insertion time: \(seconds) ms."
-    }
-
-    func insertAtEndArray() async -> String {
-        var array = myArray
-        let startTime = DispatchTime.now()
-        
-        array.insert(contentsOf: 1...1000, at: 9_999_999)
-        
-        let endTime = DispatchTime.now()
-        
-        insertedAtEndAllAtOnceResults = array
-        
-        let nanoseconds = endTime.uptimeNanoseconds - startTime.uptimeNanoseconds
-        let seconds = Double(nanoseconds) / 1_000_000_000
-        
-        return "Insertion time: \(seconds) ms."
-    }
-
-    // MARK: - Remove at Beginning
-    func removeAtBeginningArrayOneByOne() async -> String {
-        var array = myArray
-        let startTime = DispatchTime.now()
-        
-        for _ in 1...1000 {
-            array.remove(at: 0)
-        }
-        let endTime = DispatchTime.now()
-        
-        removedAtBeginningOneByOneResults = array
-        
-        let nanoseconds = endTime.uptimeNanoseconds - startTime.uptimeNanoseconds
-        let seconds = Double(nanoseconds) / 1_000_000_000
-        
-        return "Removal time: \(seconds) ms."
-    }
-
-    func removeAtBeginningArray() async -> String {
-        var array = myArray
-        let startTime = DispatchTime.now()
-        
-        array.removeFirst(1000)
-        
-        let endTime = DispatchTime.now()
-        removedAtBeginningAllAtOnceResults = array
-        
-        let nanoseconds = endTime.uptimeNanoseconds - startTime.uptimeNanoseconds
-        let seconds = Double(nanoseconds) / 1_000_000_000
-        
-        return "Removal time: \(seconds) ms."
-    }
-
-    // MARK: - Remove in Middle
-    func removeInMiddleArrayOneByOne() async -> String {
-        var array = myArray
-        let startTime = DispatchTime.now()
-        
-        for _ in 0..<1000 {
-            let middleIndex = array.count / 2
-            array.remove(at: middleIndex)
-        }
-        let endTime = DispatchTime.now()
-        
-        removedInMiddleOneByOneResults = array
-        
-        let nanoseconds = endTime.uptimeNanoseconds - startTime.uptimeNanoseconds
-        let seconds = Double(nanoseconds) / 1_000_000_000
-        
-        return "Removal time: \(seconds) ms."
-    }
-
-    func removeInMiddleArray() async -> String {
-        var array = myArray
-        let startTime = DispatchTime.now()
-        
-        let middleIndex = array.count / 2
-        
-        let startRemoveIndex = max(0, middleIndex - 500)
-        let endIndex = min(array.count, startRemoveIndex + 1000)
-        array.removeSubrange(startRemoveIndex..<endIndex)
-        
-        let endTime = DispatchTime.now()
-        removedInMiddleAllAtOnceResults = array
-        
-        let nanoseconds = endTime.uptimeNanoseconds - startTime.uptimeNanoseconds
-        let seconds = Double(nanoseconds) / 1_000_000_000
-        
-        return "Removal time: \(seconds) ms."
-    }
-
-    // MARK: - Remove at End
-    func removeAtEndArrayOneByOne() async -> String {
-        var array = myArray
-        let startTime = DispatchTime.now()
-        
-        for _ in 1...1000 {
-            array.removeLast()
-        }
-        let endTime = DispatchTime.now()
-        
-        removedAtEndOneByOneResults = array
-        
-        let nanoseconds = endTime.uptimeNanoseconds - startTime.uptimeNanoseconds
-        let seconds = Double(nanoseconds) / 1_000_000_000
-        
-        return "Removal time: \(seconds) ms."
-    }
-
-    func removeAtEndArray() async -> String {
-        var array = myArray
-        let startTime = DispatchTime.now()
-        
-        array.removeLast(1000)
-
-        let endTime = DispatchTime.now()
-        removedAtEndAllAtOnceResults = array
-        
-        let nanoseconds = endTime.uptimeNanoseconds - startTime.uptimeNanoseconds
-        let seconds = Double(nanoseconds) / 1_000_000_000
-        
-        return "Removal time: \(seconds) ms."
+        let time = endTime.uptimeNanoseconds - startTime.uptimeNanoseconds
+        return ArrayOperationResult(time: time, arraySnapshot: array)
     }
 }
