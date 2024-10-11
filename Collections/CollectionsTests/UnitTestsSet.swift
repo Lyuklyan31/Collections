2ddd7f5c9fcc3c6936930e5410a4fb6e7348137b
Oleh <lyuclyan.oleg14@gmail.com>
2024-10-11 15:29:33 +0300

    Fix Set UnitTests.

--- Collections/CollectionsTests/UnitTestsSet.swift
+++ Collections/CollectionsTests/UnitTestsSet.swift
@@ -1,40 +1,45 @@
 import XCTest
 @testable import Collections
 
-class ButtonsTests: XCTestCase {
-    
+class UnitTestsSet: XCTestCase {
+    var viewModel: SetViewModel!
+
+    override func setUp() {
+        super.setUp()
+        viewModel = SetViewModel()
+    }
+
+    override func tearDown() {
+        viewModel = nil
+        super.tearDown()
+    }
+
     // MARK: - Matching Letters Tests
     func testMatchingLetters() {
-        let button = Buttons.matchingLetters
-        
         let firstText = "hello"
         let secondText = "world"
-        
-        let result = button.perform(using: ButtonView("Test"), firstText: firstText, secondText: secondText)
+
+        let result = viewModel.findMatchingLetters(firstText: firstText, secondText: secondText)
         
         XCTAssertEqual(result, "llo")
     }
 
     // MARK: - Non-Matching Letters Tests
     func testNonMatchingLetters() {
-        let button = Buttons.nonMatchingLetters
-        
         let firstText = "hello"
         let secondText = "world"
         
-        let result = button.perform(using: ButtonView("Test"), firstText: firstText, secondText: secondText)
+        let result = viewModel.findNonMatchingLetters(firstText: firstText, secondText: secondText)
         
         XCTAssertEqual(result, "he")
     }
 
     // MARK: - Unique Non-Matching Characters Tests
     func testUniqueNonMatchingCharacters() {
-        let button = Buttons.nonMatchingUniqueChars
-        
         let firstText = "hello"
         let secondText = "world"
         
-        let result = button.perform(using: ButtonView("Test"), firstText: firstText, secondText: secondText)
+        let result = viewModel.findUniqueNonMatchingChars(firstText: firstText, secondText: secondText)
         
         let expectedSet: Set<Character> = Set("he")
         let resultSet: Set<Character> = Set(result)
@@ -44,34 +49,28 @@ class ButtonsTests: XCTestCase {
 
     // MARK: - Edge Case Tests
     func testEmptyFirstText() {
-        let button = Buttons.matchingLetters
-        
         let firstText = ""
         let secondText = "world"
         
-        let result = button.perform(using: ButtonView("Test"), firstText: firstText, secondText: secondText)
+        let result = viewModel.findMatchingLetters(firstText: firstText, secondText: secondText)
         
         XCTAssertEqual(result, "")
     }
     
     func testEmptySecondText() {
-        let button = Buttons.nonMatchingLetters
-        
         let firstText = "hello"
         let secondText = ""
         
-        let result = button.perform(using: ButtonView("Test"), firstText: firstText, secondText: secondText)
+        let result = viewModel.findNonMatchingLetters(firstText: firstText, secondText: secondText)
         
         XCTAssertEqual(result, "hello")
     }
     
     func testEmptyBothTexts() {
-        let button = Buttons.nonMatchingUniqueChars
-        
         let firstText = ""
         let secondText = ""
         
-        let result = button.perform(using: ButtonView("Test"), firstText: firstText, secondText: secondText)
+        let result = viewModel.findUniqueNonMatchingChars(firstText: firstText, secondText: secondText)
         
         XCTAssertEqual(result, "")
     }
